7c40b454bdd7cf7526fc399afe5a891d5cbfd81a
Pedro <pedromsra@gmail.com>
2022-05-03 22:31:48 -0300

    Criando e Editando Pins

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,50 @@
+class PinsController < ApplicationController
+	def index
+		@pins = Pin.all
+	end
+	
+	def show
+		@pins = Pin.find(params[:id])
+	end
+	
+	def new
+		@pins = Pin.new
+	end
+	
+	def create
+		@pins = Pin.new(pin_params)
+		
+		if @pins.save
+			redirect_to @pins, notice: "Pin criado com sucesso"
+		else
+			render :new, status: :unprocessable_entity
+		end
+	end
+	
+	def edit
+		@pins = Pin.find(params[:id])
+	end
+	
+	def update
+		@pins = Pin.find(params[:id])
+		
+		if @pins.update(pin_params)
+			redirect_to @pins, notice: "Pin alterado com sucesso"		
+		else
+			reder :edit, status: :unprocessable_entity
+		end
+	end
+	
+	def destroy
+		@pins = Pin.find(params[:id])
+		@pins.destroy
+		
+		redirect_to root_path, status: :see_other, notice: "Pin removido com sucesso"
+	end
+	
+	private
+	
+	def pin_params
+		params.require(:pin).permit(:title, :description)
+	end
+end
